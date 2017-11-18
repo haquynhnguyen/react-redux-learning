7b04d038694f49b6a02bc081df4a176cc3f623ab
qnguyentk2 <quynhnguyentk2@hotmail.com>
2017-11-18 16:05:41 +0700

    setup babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('app.js is running');
+// install -> import -> use
+// import validator from 'validator';
+import react from 'react';
+import reactDOM from 'react-dom';
+
+const template = <p>this is jsx</p>
+reactDOM.render(template, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
-// setup webpack
+// setup webpack , babel
 // wwww.webpack.js.org
 const path = require('path');
+console.log(__dirname);
 
 module.exports = {
 	entry: './src/app.js',
 	output: {
 		path: path.join(__dirname + '/public'),
 		filename: 'bundle.js'
+	}, 
+	module: {
+		rules: [{
+			loader: 'babel-loader',
+			test: /\.js$/,
+			exclude: /node_modules/
+		}]
 	}
-}
+};
